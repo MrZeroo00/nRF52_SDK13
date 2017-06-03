d7e0dee4b548b041cc67559f917a98e417808604
Badr <badr@rtone.fr>
2017-06-03 19:11:36 +0200

    Initial commit of the service_handson project (copy of the template)

---
+++ examples/ble_peripheral/service_handson/pca10040/s132/armgcc/_build/nrf52832_xxaa_sensorsim.c.d
@@ -0,0 +1,29 @@
+_build/nrf52832_xxaa_sensorsim.c.o: \
+ ../../../../../../components/libraries/sensorsim/sensorsim.c \
+ ../../../../../../components/libraries/sensorsim/sensorsim.h \
+ /Users/rtone-badr/Desktop/Projects/gcc-arm-none-eabi-6-2017-q1-update/lib/gcc/arm-none-eabi/6.3.1/include/stdint.h \
+ /Users/rtone-badr/Desktop/Projects/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/stdint.h \
+ /Users/rtone-badr/Desktop/Projects/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/_default_types.h \
+ /Users/rtone-badr/Desktop/Projects/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/features.h \
+ /Users/rtone-badr/Desktop/Projects/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/_newlib_version.h \
+ /Users/rtone-badr/Desktop/Projects/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_intsup.h \
+ /Users/rtone-badr/Desktop/Projects/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_stdint.h \
+ /Users/rtone-badr/Desktop/Projects/gcc-arm-none-eabi-6-2017-q1-update/lib/gcc/arm-none-eabi/6.3.1/include/stdbool.h
+
+../../../../../../components/libraries/sensorsim/sensorsim.h:
+
+/Users/rtone-badr/Desktop/Projects/gcc-arm-none-eabi-6-2017-q1-update/lib/gcc/arm-none-eabi/6.3.1/include/stdint.h:
+
+/Users/rtone-badr/Desktop/Projects/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/stdint.h:
+
+/Users/rtone-badr/Desktop/Projects/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/_default_types.h:
+
+/Users/rtone-badr/Desktop/Projects/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/features.h:
+
+/Users/rtone-badr/Desktop/Projects/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/_newlib_version.h:
+
+/Users/rtone-badr/Desktop/Projects/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_intsup.h:
+
+/Users/rtone-badr/Desktop/Projects/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_stdint.h:
+
+/Users/rtone-badr/Desktop/Projects/gcc-arm-none-eabi-6-2017-q1-update/lib/gcc/arm-none-eabi/6.3.1/include/stdbool.h:
